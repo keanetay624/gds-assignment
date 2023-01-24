691bbe78539129d5227c2e7971f227cf2abd5bf9
keane <keanetay@gmail.com>
2023-01-24 12:53:18 +0800

    add import for HttpClientModule

--- gds-fe/src/app/app.module.ts
+++ gds-fe/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { EmployeesListComponent } from './components/employees-list/employees-list.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavBtnComponent } from './components/nav-btn/nav-btn.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 
@@ -20,7 +21,8 @@ import { EmployeeComponent } from './components/employee/employee.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
