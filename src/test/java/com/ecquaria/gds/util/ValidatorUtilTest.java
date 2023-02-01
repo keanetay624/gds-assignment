657f080602c18bd6e6447c806d2c66088dc2944e
keane <keanetay@gmail.com>
2023-02-01 17:58:04 +0800

    Add unit test for ValidatorUtilTest

---
+++ src/test/java/com/ecquaria/gds/util/ValidatorUtilTest.java
@@ -0,0 +1,29 @@
+package com.ecquaria.gds.util;
+
+import com.ecquaria.gds.exception.InvalidSalaryFormatException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorUtilTest {
+
+    @Test
+    @DisplayName("checkSalaryFormat should return throw invalidSalaryException if salary < 0")
+    void testCheckSalaryFormat() {
+        assertThrows(InvalidSalaryFormatException.class,() -> ValidatorUtil.checkSalaryFormat("-2"));
+    }
+
+    @Test
+    @DisplayName("checkSalaryFormat should return throw invalidSalaryException if not double format")
+    void testCheckSalaryFormat2() {
+        assertThrows(InvalidSalaryFormatException.class,() -> ValidatorUtil.checkSalaryFormat("k9"));
+    }
+
+    @Test
+    @DisplayName("checkSalaryFormat should return throw invalidSalaryException if min sal > max sal")
+    void testCheckSalaryFormat3() {
+        assertThrows(InvalidSalaryFormatException.class,() -> ValidatorUtil.checkSalaryFormat("2","1"));
+    }
+
+}
